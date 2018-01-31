f71a0eb2a19e86c482c0a6cdd6e48a9766a6f69b
DeltaF1 <delyaerricson@gmail.com>
2018-01-30 23:39:38 -0500

    Added leave command, improved rolling

--- chad.py
+++ chad.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-import time, re, random
+import time, re, random, os, sys
 import threading
 from queue import Queue
 import json
@@ -10,8 +10,16 @@ from fbchat.models import *
 
 import thesaurus
 
-
-
+#from Markus Jarderot @ https://stackoverflow.com/questions/597476
+class Re(object):
+  def __init__(self):
+    self.last_match = None
+  def match(self,pattern,text):
+    self.last_match = re.match(pattern,text)
+    return self.last_match
+  def search(self,pattern,text):
+    self.last_match = re.search(pattern,text)
+    return self.last_match
 
 
 def response_loop():
@@ -38,10 +46,11 @@ def response_loop():
 
 def parse_message(self, mid, author_id, message, message_object, thread_id, thread_type, ts, metadata, msg):
     text = message_object.text
-    match = virgin_re.match(message_object.text.lower())
+    
+    gre = Re()
 
-    if match:
-        word = match.group(1)
+    if gre.match(virgin_re, text.lower()):
+        word = gre.last_match.group(1)
         synonyms = thesaurus.get_synonyms(word)
 
         try:
@@ -57,41 +66,48 @@ def parse_message(self, mid, author_id, message, message_object, thread_id, thre
 
             
             responses.put((response, thread_id, thread_type))
-    elif message_object.text == "STOP IT CHAD":
+    elif author_id == config["facebook"]["owner_uid"] and message_object.text == "STOP IT CHAD":
         responses.put(("Ouch!", thread_id, thread_type))
         #self.send(Message(text="Ouch!"), thread_id=thread_id, thread_type=thread_type)
         self.stopListening()
+    elif text == "BEGONE CHAD!":
+        exit_message = "Chad strides into the sunset, never to be seen again"
+        self.send(Message(text=exit_message, mentions=[Mention(self.uid, 0, len(exit_message))]), thread_id, thread_type)
+        self.removeUserFromGroup(self.uid, thread_id)
+    elif gre.search(dice_re, text.lower()):
+        match = gre.last_match
+        num_dice = int(match.group(1) or '1')
+        dice_type = int(match.group(2))
+        if match.group(3):
+            constant = int(match.group(3))
+        else:
+            constant = 0
         
-    elif message_object.text.lower().startswith("@chad"):
-        match = dice_re.search(text.lower())
-        
-        
-        
-        if match:
-            num_dice = int(match.group(1))
-            dice_type = int(match.group(2))
-            if match.group(3):
-                constant = int(match.group(3))
-            else:
-                constant = 0
-            
-            total = 0
-            if num_dice > 0 and num_dice < 200:
-                for i in range(num_dice):
-                    total += random.randint(1, dice_type)
-            
-            total += constant
-            
-            user = self.fetchUserInfo(author_id)[author_id]
-            
-            print("Rolling dice for " + (user.nickname or user.first_name))
-            
-            name = "@"+(user.nickname or user.first_name)
-            
-            response = name + " rolled "+str(total)
-            
-            responses.put((Message(text=response, mentions=[Mention(author_id, 0, len(name))]), thread_id, thread_type))
+        total = 0
+        if dice_type > 0 and num_dice > 0 and num_dice <= 200:
+            for i in range(num_dice):
+                total += random.randint(1, dice_type)
+        else:
             return
+            
+        total += constant
+        
+        user = self.fetchUserInfo(author_id)[author_id]
+        
+        print("Rolling dice for " + (user.nickname or user.first_name))
+        
+        name = "@"+(user.nickname or user.first_name)
+        
+        try:
+            #gets the rest of the text following the end of the roll command
+            reason = text[match.span()[1]:]
+        except IndexError:
+            reason = ""
+        
+        response = name + " rolled " + str(total) + reason
+        
+        responses.put((Message(text=response, mentions=[Mention(author_id, 0, len(name))]), thread_id, thread_type))
+        return
 
 class Chad(Client):
     active = True
@@ -110,7 +126,7 @@ if __name__ == '__main__':
     owner_uid = config["facebook"]["owner_uid"]
 
     virgin_re = re.compile("the virgin (.*)");
-    dice_re = re.compile("roll ([0-9]+)d([0-9])+(?: *\+ *([0-9]+))?")
+    dice_re = re.compile("roll ([0-9]*)d([0-9]+)(?: *\+ *([0-9]+))?")
     
     responses = Queue()
     
@@ -122,7 +138,5 @@ if __name__ == '__main__':
     response_thread.start()
 
     chad.listen()
-
-    time.sleep(5)
 
     chad.logout()
