45268dd23c1b88be559f0f59b01e85a5cafa9614
DeltaF1 <delyaerricson@gmail.com>
2018-01-31 23:15:15 -0500

    Added alternative data-source

---
+++ datamuse.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+with open("conf.json", "r") as f:
+    config = json.load(f)
+    
+url = "https://api.datamuse.com/words"
+
+def get_synonyms(word):
+    r = requests.get(url, params =
+    {
+        "ml" : word,
+        "md" : "f",
+    })
+    
+    print(r.url)
+    
+    words = r.json()
+    
+    return [word["word"] for word in words[0:10]]
+    
+def get_frequency(data):
+    tags = data["tags"]
+    for string in tags:
+        if string.startswith("f:"):
+            return float(string.split(":")[1])
+           
+    
+def chadlier(word):
+    synonyms = get_synonyms(word)
+    
+    #synonyms.sort(key = get_frequency)
+    
+    weirdest = synonyms[0:10]
+    
+    return weirdest
